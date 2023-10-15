e80bdf0ecce3a302d28b49647b336c07a4c73e7f
Kyuba98 <jakub.krzeminski1998@gmail.com>
2023-10-15 20:23:05 +0200

    Dodano formularz, oraz poprawiono image

---
+++ Controllers/FormularzController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class FormularzController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(string name, string lname, string city)
+        {
+            ViewData["name"] = name ?? "Imie";
+            ViewData["lname"] = lname ?? "Nazwisko";
+            ViewData["city"] = city ?? "Miasto";
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Index")]
+        public IActionResult IndexPost(string name, string lname, string city)
+        {
+            ViewData["name"] = name;
+            ViewData["lname"] = lname;
+            ViewData["city"] = city;
+            return RedirectToAction("Confirm", new { name, lname, city });
+        }
+        public IActionResult Confirm(string name, string lname, string city)
+        {
+            ViewData["name"] = name;
+            ViewData["lname"] = lname;
+            ViewData["city"] = city;
+
+            return View();
+        }
+    }
+}
